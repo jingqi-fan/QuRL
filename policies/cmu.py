804854c8540316adf2b60f88f305787ab542fe47
jqfan <jingqi_fan@163.com>
2025-11-09 20:20:01 +0800

    1

--- policies/cmu.py
+++ policies/cmu.py
@@ -3,5 +3,11 @@ class MaxWeightCMuPolicy:
         pass
 
     def test_forward(self, step, batch_queue, batch_time, repeated_queue, repeated_network, repeated_mu, repeated_h):
+        print(f'------------------------cmu------------------------------')
+        # print(f'repeated_queue: {repeated_queue}')
+        # print(f'repeated_network: {repeated_network}')
+        print(f'repeated_mu: {repeated_mu}')
+        print(f'repeated_h: {repeated_h}')
+        print(f'return {repeated_h * repeated_mu}')
         return repeated_h * repeated_mu
 
--- policies/max_weight.py
+++ policies/max_weight.py
@@ -3,5 +3,11 @@ class MaxWeightCMuQPolicy:
         pass
 
     def test_forward(self, step, batch_queue, batch_time, repeated_queue, repeated_network, repeated_mu, repeated_h):
+        print(f'------------------------max-weight------------------------------')
+        print(f'repeated_queue: {repeated_queue}')
+        print(f'repeated_network: {repeated_network}')
+        print(f'repeated_mu: {repeated_mu}')
+        print(f'repeated_h: {repeated_h}')
+        print(f'return {repeated_queue * repeated_h * repeated_network * repeated_mu}')
         return repeated_queue * repeated_h * repeated_network * repeated_mu
 
